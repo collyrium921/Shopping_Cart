b297eba87e628a798da515112448660310b44211
Kajal Singhal <kajusinghal921@gmail.com>
2022-08-25 15:33:11 +0530

    ui of list and card is complete

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from '../app.service';
-import { LIST_ITEMS } from '../listItems';
+import { Component, OnInit } from '@angular/core'
+import { AppService } from '../app.service'
+import { LIST_ITEMS } from '../listItems'
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  isListFormat: boolean = false
   listItems = LIST_ITEMS
-  item:any=[]
+  item: any = []
   constructor(private appService: AppService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  addItemToCart(i:number, shoe: any){
-      this.item.push({
-        name:shoe.name,
-        price:shoe.price
-      })
-      this.appService.addedItem.next(this.item[this.item.length-1])
+  addItemToCart(i: number, shoe: any) {
+    this.item.push({
+      name: shoe.name,
+      price: shoe.price,
+    })
+    this.appService.addedItem.next(this.item[this.item.length - 1])
   }
 }
